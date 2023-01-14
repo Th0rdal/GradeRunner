17cf6d0e938b813dfd09fba90acf1a60ec236792
lw81 <>
2023-01-14 17:45:43 +0100

    Mute Button added to Menu and PauseMenu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -7,33 +7,32 @@ public class Menu extends MouseAdapter{
 
   private Game game;
   private Handler handler;
-  private Random r = new Random();
-  private Color color;
 
   private boolean mouseOverButton1 = false;
   private boolean mouseOverButton2 = false;
   private boolean mouseOverButton3 = false;
+  private boolean mouseOverButtonMute = false;
 
 
   public Menu(Game game, Handler handler){
     this.game = game;
     this.handler = handler;
-
   }
 
+  public void pass(){}
 
-  public void tick() {
-
-  }
+  public void tick(){}
 
   public void render(Graphics g) {
     Font menu = new Font("arial", 1, 100);
-    Font buttons = new Font("arial", 1, 70);
-    Font buttonsAlt = new Font("arial", 1, 80);
-    Font buttons2 = new Font("arial", 1, 70);
-    Font buttons2Alt = new Font("arial", 1, 80);
-    Font buttons3 = new Font("arial", 1, 70);
-    Font buttons3Alt = new Font("arial", 1, 80);
+    Font button = new Font("arial", 1, 70);
+    Font buttonAlt = new Font("arial", 1, 80);
+    Font button2 = new Font("arial", 1, 70);
+    Font button2Alt = new Font("arial", 1, 80);
+    Font button3 = new Font("arial", 1, 70);
+    Font button3Alt = new Font("arial", 1, 80);
+    Font muteButton = new Font("arial", 1, 30);
+    Font muteButtonAlt = new Font("arial", 1, 40);
 
 
     g.setFont(menu);
@@ -59,33 +58,45 @@ public class Menu extends MouseAdapter{
     }
     g.fillRect(375, 500, 500, 100);
     //g.drawRect(375, 700, 500, 100);
+    if (this.mouseOverButtonMute) {
+      g.setColor(Color.gray);
+    } else {
+      g.setColor(Color.black);
+    }
+    g.fillOval(1100,50,50,50);
 
     g.setColor(Color.white);
 
-
     if (this.mouseOverButton1) {
-      g.setFont(buttonsAlt);
+      g.setFont(buttonAlt);
       g.drawString("Play", 545, 275);
     } else {
-      g.setFont(buttons);
+      g.setFont(button);
       g.drawString("Play", 555, 275);
     }
 
-
     if (this.mouseOverButton2) {
-      g.setFont(buttons2Alt);
+      g.setFont(button2Alt);
       g.drawString("Level-Select", 390, 425);
     } else {
-      g.setFont(buttons2);
+      g.setFont(button2);
       g.drawString("Level-Select", 425, 425);
     }
 
     if (this.mouseOverButton3) {
-      g.setFont(buttons3Alt);
+      g.setFont(button3Alt);
       g.drawString("Exit", 545, 575);
     } else {
-      g.setFont(buttons3);
+      g.setFont(button3);
       g.drawString("Exit", 555, 575);
+    }
+
+    if (this.mouseOverButtonMute) {
+      g.setFont(muteButtonAlt);
+      g.drawString("M", 1109, 90);
+    } else {
+      g.setFont(muteButton);
+      g.drawString("M", 1112, 85);
     }
 
     //g.drawString("Quit", 555, 775);
@@ -107,6 +118,7 @@ public class Menu extends MouseAdapter{
     this.mouseOverButton1 = false;
     this.mouseOverButton2 = false;
     this.mouseOverButton3 = false;
+    this.mouseOverButtonMute = false;
 
     if (mouseOverBox(mx, my, 375, 200, 500, 100)) {
       this.mouseOverButton1 = true;
@@ -114,6 +126,8 @@ public class Menu extends MouseAdapter{
       this.mouseOverButton2 = true;
     } else if (mouseOverBox(mx, my, 375, 500, 500, 100)) {
       this.mouseOverButton3 = true;
+    }else if (mouseOverBox(mx, my, 1100,50,50,50)) {
+      this.mouseOverButtonMute  = true;
     }
   }
 
@@ -132,6 +146,9 @@ public class Menu extends MouseAdapter{
       if (mouseOverBox(mx, my, 375, 350, 500, 100)) {
         //game.gamestate = Game.STATE.Levelselect;
         game.setGamestate(Game.STATE.Levelselect);
+      }
+      if (mouseOverBox(mx, my, 1100,50,50,50)) {
+        pass();
       }
 
 //      if (mouseOverBox(mx, my, 375, 500, 500, 100)) {
